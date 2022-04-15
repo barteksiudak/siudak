cc89150e2d8ff16c68fdaffea10a3b7e6445942c
Batek Siudak <bartek@siudak.com>
2022-04-15 16:12:13 +0200

    removed britenet

---
+++ out/_next/static/vkZQhxxUJJDmlKncx0hsj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/338-f50630346f3a4e3c1cf8.js","static/chunks/pages/index-1b25f176111c506cfd6e.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
